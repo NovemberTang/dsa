7e57f09ee05bd80c224261ce9acb490e8c5e3424
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-07-04 12:09:51 +0100

    fix merge function

--- hello.c
+++ hello.c
@@ -14,21 +14,49 @@ int findIndex(int *arr, int value, int arr_len)
         }
     }
     printf("Couldn't find value %d in array\n", value);
-    return -1;
+    return -1; // couldn't find the value.
 }
 
-void *mergeSortedArrays(int *xs, int x_mem, int *ys, int y_mem)
+void mergeSortedArrays(int *xs, int x_len, int *ys, int y_len)
 {
     int z_idx = 0;
-    int zs[x_mem + y_mem];
+    int zs[x_len + y_len];
 
     int x_idx = 0;
     int y_idx = 0;
 
     // while ((x_mem + 1 > x_idx) && (y_mem + 1 > y_idx))
-    while (z_idx < x_mem + y_mem)
+    while (z_idx < x_len + y_len)
     {
-        if (xs[x_idx] <= ys[y_idx] && xs[x_idx] != 0) // e.g. x = 1 , y = 2, merge x.
+        if (x_idx >= x_len && y_idx >= y_len)
+        {
+            printf("Reached the end of both arrays. Terminating.\n");
+            break;
+        }
+        else if (x_idx >= x_len)
+        {
+            printf("Reached the end of x array. Merging the rest of y array.\n");
+            while (y_idx < y_len)
+            {
+                zs[z_idx] = ys[y_idx];
+                z_idx++;
+                y_idx++;
+            }
+            printArray(zs, x_len + y_len);
+            break;
+        }
+        else if (y_idx >= y_len)
+        {
+            printf("Reached the end of y array. Merging the rest of x array.\n");
+            while (x_idx < x_len)
+            {
+                zs[z_idx] = xs[x_idx];
+                z_idx++;
+                x_idx++;
+            }
+            break;
+        }
+        else if (xs[x_idx] <= ys[y_idx] && x_idx < x_len) // e.g. x = 1 , y = 2, merge x.
         {
             int x = xs[x_idx];
             printf("Adding element %d, value %d  from xs to merged array\n", x_idx, x);
@@ -38,7 +66,7 @@ void *mergeSortedArrays(int *xs, int x_mem, int *ys, int y_mem)
             x_idx++;
             printf("%d\n\n", x_idx);
         }
-        else if (xs[x_idx] > ys[y_idx] && ys[y_idx] != 0) // e.g. x=2, y = 1, merge y.
+        else // if (xs[x_idx] > ys[y_idx] && y_idx < y_len) // e.g. x=2, y = 1, merge y.
         {
             int y = ys[y_idx];
             printf("Adding element %d, value %d from ys to merged array\n", y_idx, y);
@@ -46,36 +74,20 @@ void *mergeSortedArrays(int *xs, int x_mem, int *ys, int y_mem)
             printf("Element %d: %d\n", z_idx, zs[z_idx]);
             z_idx++;
             y_idx++;
-            printf("y_mem: %d\n", y_mem);
+            printf("y_mem: %d\n", y_len);
             printf("y_idx: %d\n\n", y_idx);
-        }
-        else if (xs[x_idx] != ys[x_idx] && (xs[x_idx] == 0 || ys[y_idx] == 0)) // one of them is equal to zero, add the other.
-        {
-            printf("One element is zero, adding the other\n");
-            int x_is_zero = xs[x_idx] == 0;
-            int z = x_is_zero ? ys[y_idx] : xs[x_idx];
-            char *chosen_array = x_is_zero ? "ys" : "xs";
-            int chosen_elem = x_is_zero ? y_idx : x_idx;
-            printf("Adding element %d, value %d from %s to array\n\n", chosen_elem, z, chosen_array);
-            zs[z_idx] = z;
-            z_idx++;
-            x_is_zero ? y_idx++ : x_idx++;
-        }
-        else // both of them are equal to 0, terminate the loop.
-        {
-            break;
         }
     }
     printf("x index: %d. y index: %d\n", x_idx, y_idx);
 
-    printf("Terminating.\nx_idx: %d, x_mem: %d\ny_idx: %d, y_mem: %d\n", x_idx, x_mem, y_idx, y_mem);
-    printArray(zs, x_mem + y_mem);
+    printf("Terminating.\nx_idx: %d, x_mem: %d\ny_idx: %d, y_mem: %d\n", x_idx, x_len, y_idx, y_len);
+    printArray(zs, x_len + y_len);
 }
 
 int main()
 {
 
-    int myLongArray[10] = {2,
+    int myLongArray[10] = {0,
                            4,
                            8,
                            10};
