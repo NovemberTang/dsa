eeeaad73b6160bd91ac166a1f09551ae0060f1f6
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-07-18 09:57:14 +0100

    simple matrix functions

--- hello.c
+++ hello.c
@@ -18,12 +18,50 @@ int findIndex(int *arr, int value, int arr_len)
     return -1; // couldn't find the value.
 }
 
-int main()
+void twoDimArraySum(int arr1[3][3], int arr2[3][3])
 {
-    int twoDimArray[2][2] = {{1, 2}, {3, 4}};
+    int resultArray[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            resultArray[i][j] = arr1[i][j] + arr2[i][j];
+        }
+        printArray(resultArray[i], 3);
+    }
+};
 
+void transpose(int arr[3][3])
+{
+    int resultArray[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            resultArray[i][j] = arr[j][i];
+        }
+        printArray(resultArray[i], 3);
+    }
+}
+
+void print3dArray(int arr[2][2][2])
+{
     for (int i = 0; i < 2; i++)
     {
-        printArray(twoDimArray[i], 2);
+        for (int j = 0; j < 2; j++)
+        {
+            printArray(arr[i][j], 2);
+        }
     }
+}
+
+int main()
+{
+    int twoDimArray[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    int twoDimArray2[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+
+    int threeDimArray[2][2][2] = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}};
+
+    print3dArray(threeDimArray);
 }
