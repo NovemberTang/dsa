a10e19e548caabc8bec00f6c30c149da3b0d2450
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-12-19 13:30:29 +0000

    move multidimensional array code

--- arrayUtils.c
+++ arrayUtils.c
@@ -19,3 +19,41 @@ void printArray(int *arr, int arraySize)
     }
     printf("\n");
 }
+
+
+void twoDimArraySum(int arr1[3][3], int arr2[3][3])
+{
+    int resultArray[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            resultArray[i][j] = arr1[i][j] + arr2[i][j];
+        }
+        printArray(resultArray[i], 3);
+    }
+}
+
+void transpose(int arr[3][3])
+{
+    int resultArray[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            resultArray[i][j] = arr[j][i];
+        }
+        printArray(resultArray[i], 3);
+    }
+}
+
+void print3dArray(int arr[2][2][2])
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printArray(arr[i][j], 2);
+        }
+    }
+}
--- hello.c
+++ hello.c
@@ -20,43 +20,6 @@ int findIndex(int *arr, int value, int arr_len)
     return -1; // couldn't find the value.
 }
 
-void twoDimArraySum(int arr1[3][3], int arr2[3][3])
-{
-    int resultArray[3][3];
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            resultArray[i][j] = arr1[i][j] + arr2[i][j];
-        }
-        printArray(resultArray[i], 3);
-    }
-}
-
-void transpose(int arr[3][3])
-{
-    int resultArray[3][3];
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            resultArray[i][j] = arr[j][i];
-        }
-        printArray(resultArray[i], 3);
-    }
-}
-
-void print3dArray(int arr[2][2][2])
-{
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            printArray(arr[i][j], 2);
-        }
-    }
-}
-
 struct ringBuffer
 {
     int lastReadAddress;
