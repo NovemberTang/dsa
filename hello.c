fe235d0d5b18657233d967d861ea81774e49c077
Natasha <67543397+NovemberTang@users.noreply.github.com>
2025-05-29 11:56:47 +0100

    wip: create a basic hashmap with chaining for conflicts

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@
 #include "binarySearchTree.h"
 #include <math.h>
 #include <string.h> //for strcpy
+#include "linkedList.h"
 
 
 void in_order_traversal(struct tree_node* root){
@@ -15,9 +16,48 @@ void in_order_traversal(struct tree_node* root){
     }
 }
 
+int hash(int x){
+    return x % 5;
+}
+
+void add_to_hashmap(int x, struct node **arr ){
+    int idx = hash(x);
+    struct node* node_at_index = arr[idx];
+
+    struct node *newNode = malloc(sizeof(struct node));
+    *newNode = (struct node){x, NULL};
+
+    if(node_at_index==NULL){//nothing exists with that hash currently
+        node_at_index=newNode;
+        printf("put %d in index %d\n", x, idx);
+    }else{ //if there is a hash collision
+        while(node_at_index->next != NULL){
+            node_at_index=node_at_index->next;
+        }
+        node_at_index->next = newNode;
+        printf("put %d in index %d, after %d\n", x, idx, node_at_index->value);
+    }
+}
+
 
 int main()
 {
+
+    //4,14
+
+
+    struct node* arr[5];
+
+    for(int i=0;i<5;i++){
+        arr[i]=NULL;
+    }
+
+    printf("created array.\n");
+
+    add_to_hashmap(4, arr);
+    add_to_hashmap(14, arr);
+
+    
 
     return 0;
 
