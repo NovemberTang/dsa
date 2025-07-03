b9f835957508ed43574b097bd665b2e3a30f6561
Natasha <67543397+NovemberTang@users.noreply.github.com>
2025-07-03 12:11:51 +0100

    adjacency list representation of a graph

--- graphs.py
+++ graphs.py
@@ -21,3 +21,25 @@ matrix[nodemap["A"]][nodemap["A"]]= 1
 
 for i in matrix:
     print(i)
+
+#adjacency list representation of a graph
+
+class LlNode:
+    def __init__(self, vertex):
+        self.vertex = vertex
+    vertex: str
+    next: 'LlNode' = None
+
+graph = {
+    "A": LlNode("A"),
+    "B": LlNode("B"),
+    "C": LlNode("C"),
+    "D": LlNode("D")
+    }
+
+graph["A"].next = LlNode("B")
+graph["A"].next.next = LlNode("C")
+
+print(graph["A"].next.vertex)
+print(graph["A"].next.next.vertex)
+print(graph["A"].next.next.next)
