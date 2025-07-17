a27a368153d1fa02c8ab3547b7e0ca3a3f40a69f
Natasha <67543397+NovemberTang@users.noreply.github.com>
2025-07-17 11:45:37 +0100

    make bfs its own function

--- graphs.py
+++ graphs.py
@@ -30,48 +30,46 @@ class LlNode:
     vertex: str
     next: 'LlNode' = None
 
-graph = {
-    "A": LlNode("A"),
-    "B": LlNode("B"),
-    "C": LlNode("C"),
-    "D": LlNode("D")
-    }
+def graph_bfs():
 
+    graph = {
+        "A": LlNode("A"),
+        "B": LlNode("B"),
+        "C": LlNode("C"),
+        "D": LlNode("D")
+        }
 
+    graph["A"].next = LlNode("B")
+    graph["A"].next.next = LlNode("C")
+    graph["B"].next = LlNode("C")
+    graph["C"].next = LlNode("D")
+    graph["D"].next = LlNode("B")
 
-graph["A"].next = LlNode("B")
-graph["A"].next.next = LlNode("C")
-graph["B"].next = LlNode("C")
-graph["C"].next = LlNode("D")
-graph["D"].next = LlNode("B")
+    queue = []
+    visited = set()
+    queue.append("A")
 
-queue = []
+    while len(queue)>0:
+        print("\n")
 
-visited = set()
+        current_node = queue.pop(0)
+        print("current node: " + current_node)
+        if current_node in visited:
+            print("already visited: " + current_node)
+            continue
+        else:
+            adjacent_node = graph[current_node].next
+            while adjacent_node != None:
+                if adjacent_node.vertex not in visited:
+                    print("adding " + adjacent_node.vertex + " to queue")
+                    queue.append(adjacent_node.vertex)
+                else:
+                    print("already visited: " + adjacent_node.vertex)
+                adjacent_node = adjacent_node.next
 
-queue.append("A")
+            print("marking " + current_node + " as visited")
+            visited.add(current_node)
 
-while len(queue)>0:
-    print("\n")
+            print(queue)
 
-    current_node = queue.pop(0)
-    print("current node: " + current_node)
-    if current_node in visited:
-        print("already visited: " + current_node)
-        continue
-    else:
-        adjacent_node = graph[current_node].next
-        while adjacent_node != None:
-            if adjacent_node.vertex not in visited:
-                print("adding " + adjacent_node.vertex + " to queue")
-                queue.append(adjacent_node.vertex)
-            else:
-                print("already visited: " + adjacent_node.vertex)
-            adjacent_node = adjacent_node.next
-
-        print("marking " + current_node + " as visited")
-        visited.add(current_node)
-
-        print(queue)
-
-print(queue)
+    print(queue)
